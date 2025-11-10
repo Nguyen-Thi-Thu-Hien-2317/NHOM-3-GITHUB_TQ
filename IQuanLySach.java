1ddc239bdf5b77be7585228084434b38dc4cbe48
lethitrucha2808-cmyk <lethitrucha2808@gmail.com>
2025-11-10 19:11:44 +0700

    Add files via upload

---
+++ IQuanLySach.java
@@ -0,0 +1,7 @@
+public interface IQuanLySach {
+    void themSach(SachTuan8 sach);
+    SachTuan8 timKiemSach(String maSach);
+    void xoaSachTheoMa(String maSach);
+    void capNhatSoLuong(String maSach, int soLuongMoi);
+    void hienThiDanhSachSach();
+}
---
+++ SachTieuThuyetTuan8.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class SachTieuThuyetTuan8 extends SachTuan8 {
+    private boolean laSachSeries;
+
+    public SachTieuThuyetTuan8() {
+        super();
+    }
+
+    public SachTieuThuyetTuan8(String maSach, String tieuDe, String tacGia, int namXuatBan,
+                          int soLuong, double giaCoBan, boolean laSachSeries) {
+        super(maSach, tieuDe, tacGia, namXuatBan, soLuong, giaCoBan);
+        this.laSachSeries = laSachSeries;
+    }
+    public boolean isLaSachSeries() {
+        return laSachSeries;
+    }
+
+    public void setLaSachSeries(boolean laSachSeries) {
+        this.laSachSeries = laSachSeries;
+    }
+    @Override
+    public double tinhGiaBan() {
+        // Nếu là sách series thì cộng thêm 15.000 VND
+        return giaCoBan + (laSachSeries ? 15000 : 0);
+    }
+    @Override
+    public boolean kiemTraTonKho(int soLuongToiThieu) {
+        return soLuong >= soLuongToiThieu;
+    }
+
+    @Override
+    public void capNhatViTri(String viTriMoi) {
+        System.out.println(" Đã chuyển sách \"" + tieuDe + "\" đến khu vực: " + viTriMoi);
+    }
+    public void nhapThongTin(Scanner sc) {
+        super.nhapThongTinChung(sc);
+        System.out.print("Sach co phai la series khong (true/false): ");
+        laSachSeries = Boolean.parseBoolean(sc.nextLine());
+    }
+    @Override
+    public String toString() {
+        return super.toString() +
+               ", La sach series: " + (laSachSeries ? "Co" : "Khong") +
+               ", Gia ban: " + tinhGiaBan() + " VND";
+    }
+}
